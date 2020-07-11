25680d8e83b3443ff2129bd5ae27fe144f80f032
Rafiqul Islam <rafiqul.islam@s2018.tu-chemnitz.de>
2020-07-11 20:18:09 +0200

    Added read_RTC() + get_student_access_time() function

--- RTC.cpp
+++ RTC.cpp
@@ -17,3 +17,25 @@ class RealTimeClock{
     void get_student_access_time();
 };
 
+
+void RealTimeClock::set_attendence_time(int h, int m){
+     attendence_hour=h;
+     attendence_minute=m;
+     
+}
+
+
+void RealTimeClock::read_RTC(){
+     //Read time from the RTC
+     //This functionality will be changed after adding the RTC
+     //Currently static time are used because missing of RTC   
+     Serial.println("Read RTC");
+     student_access_hour=9;
+     student_access_minute=0; 
+}
+
+void RealTimeClock::get_student_access_time(){
+     Serial.print("Your Access Time is ");
+     String str= String(student_access_hour) + " : " + String(student_access_minute);
+     Serial.println(str);
+} 
