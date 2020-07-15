e2f1fc7e1f77150b67d278c600472e5fcbf7ec7e
Rafiqul Islam <rafiqul.islam@s2018.tu-chemnitz.de>
2020-07-15 23:02:55 +0200

    Bux fixes + add AccessTimeVerification functionalities

--- RTC.cpp
+++ RTC.cpp
@@ -8,13 +8,17 @@ class RealTimeClock{
     
   public:
 
-    int student_access_hour;
-    int student_access_minute;
-
+    struct student_access_time{
+        int student_access_hour;
+        int student_access_minute;  
+    };
+    
+    student_access_time stdAcs;
     //Attendence time is set by the teacher
     void set_attendence_time(int, int);
     void read_RTC();
     void get_student_access_time();
+    bool verifyAccessTime(int,int);
 };
 
 
@@ -29,13 +33,25 @@ void RealTimeClock::read_RTC(){
      //Read time from the RTC
      //This functionality will be changed after adding the RTC
      //Currently static time are used because missing of RTC   
-     Serial.println("Read RTC");
-     student_access_hour=9;
-     student_access_minute=0; 
+     //Serial.println("Read RTC");
+     stdAcs.student_access_hour=9;
+     stdAcs.student_access_minute=0; 
 }
 
 void RealTimeClock::get_student_access_time(){
      Serial.print("Your Access Time is ");
-     String str= String(student_access_hour) + " : " + String(student_access_minute);
+     String str= String(stdAcs.student_access_hour) + " : " + String(stdAcs.student_access_minute);
      Serial.println(str);
 } 
+
+
+bool RealTimeClock::verifyAccessTime(int h,int m){
+  if(h<=attendence_hour && m<=attendence_minute){
+    return true;  
+  }
+
+  else{
+    return false;  
+  }
+  
+}
